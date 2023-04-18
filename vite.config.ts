dc9438865764ab10fe0fdde973f77b5ebb450f78
byte8163264 <byte8163264@gmail.com>
2023-04-18 21:46:12 +0900

    Add emotion.js library

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,25 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      plugins: [
+        [
+          '@swc/plugin-emotion',
+          {
+            // default is true. It will be disabled when build type is production.
+            sourceMap: true,
+            // default is 'dev-only'.
+            autoLabel: 'dev-only',
+            // default is '[local]'.
+            // Allowed values: `[local]` `[filename]` and `[dirname]`
+            labelFormat: 'filename',
+          },
+        ],
+      ],
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
