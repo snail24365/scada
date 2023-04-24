fa93317626176782dc28847f8b7686e8e33a0575
byte8163264 <byte8163264@gmail.com>
2023-04-24 19:33:40 +0900

    Add recoil atom
    decided to use recoil to reduce the complexity of React.Context

---
+++ src/features/scada/atom/scadaAtom.ts
@@ -0,0 +1,99 @@
+import { RefObject } from 'react';
+import { atom, selector } from 'recoil';
+import { Vector2 } from 'three';
+
+export const gridUnitState = atom({
+  key: 'gridUnit',
+  default: 10,
+});
+
+export const viewboxState = atom({
+  key: 'viewbox',
+  default: {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+  },
+});
+
+export const viewportState = atom({
+  key: 'viewport',
+  default: {
+    width: 0,
+    height: 0,
+  },
+});
+
+export const containerRefState = atom<RefObject<SVGSVGElement>>({
+  key: 'containerRef',
+  default: { current: null },
+});
+
+export const clampFunc = selector({
+  key: 'clamp',
+  get: ({ get }) => {
+    const unit = get(gridUnitState);
+    return (value: number) => Math.floor(value / unit) * unit;
+  },
+});
+
+export const getXYFunc = selector({
+  key: 'getXY',
+  get: ({ get }) => {
+    const viewbox = get(viewboxState);
+    const viewport = get(viewportState);
+    const containerRef = get(containerRefState);
+
+    return (e: React.MouseEvent<Element, MouseEvent>) => {
+      const container = containerRef.current;
+      if (!container) {
+        return new Vector2(0, 0);
+      }
+
+      const { x: offsetX, y: offsetY } = container.getBoundingClientRect();
+      const { clientX, clientY } = e;
+      const x = viewbox.x + (clientX - offsetX) * (viewbox.width / viewport.width);
+      const y = viewbox.y + (clientY - offsetY) * (viewbox.height / viewport.height);
+      return new Vector2(x, y);
+    };
+  },
+});
+
+export const isEditingState = atom({
+  key: 'isEditing',
+  default: false,
+});
+
+export const scadaEditUtil = selector({
+  key: 'scadaEditUtil',
+  get: ({ get }) => {
+    return {
+      viewport: get(viewportState),
+      viewbox: get(viewboxState),
+      gridUnit: get(gridUnitState),
+      containerRef: get(containerRefState),
+      clamp: get(clampFunc),
+      getXY: get(getXYFunc),
+      isEditing: get(isEditingState),
+    };
+  },
+});
+
+export const selectionMousedownState = atom({
+  key: 'selectionMousedown',
+  default: {
+    x: 0,
+    y: 0,
+  },
+});
+
+export const selectionBoxState = atom({
+  key: 'selectBox',
+  default: {
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+  },
+});
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+/// <reference types="vite-plugin-svgr/client" />
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux/es/exports';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 import './index.css';
+import store from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
-  <App />,
-  // </React.StrictMode>,
+  <RecoilRoot>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </RecoilRoot>,
 );
